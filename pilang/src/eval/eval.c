d34099927e7c126ccfe810bf696a99e57dce1c62
ICEY <crazycz.cc@ckx-ily.cn>
2018-09-14 22:33:25 +0800

    SBDYJ*2

--- pilang/src/eval/eval.c
+++ pilang/src/eval/eval.c
@@ -31,19 +31,6 @@ typedef struct {
   int16_t pvt;
 } plregobj_t;
 
-static plregobj_t maybe_fetch(plregobj_t maybe_onstack) {
-  if (maybe_onstack.roc != ROC_ONSTACK) {
-    return maybe_onstack;
-  }
-  
-  plregobj_t ret;
-  ret.roc = INREG;
-  ret.pvt = maybe_onstack.pvt;
-  plstkobj_t *stkobj = (plstkobj_t*)maybe_onstack.data.pvalue;
-  ret.data = stkobj->data;
-  return ret;
-}
-
 void eval_ast(ast_node_base_t *program) {
   (void)program;
 }
