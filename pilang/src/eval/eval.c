b146f56f621b33d65fcca6862b6f739c0ace0f52
ICEY <crazycz.cc@ckx-ily.cn>
2018-09-14 22:32:51 +0800

    SBDYJ

--- pilang/src/eval/eval.c
+++ pilang/src/eval/eval.c
@@ -9,6 +9,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+  ROC_INREG,
+  ROC_ONSTACK,
+  ROC_ONHEAP,
+  ROC_NONE
+} plregobj_cont_t;
+
+typedef enum {
+  PT_INT,
+  PT_FLOAT,
+  PT_STR,
+  PT_LIST,
+  PT_REF,
+  PT_UNDEFINED
+} pl_value_type_t;
+
+typedef struct {
+  jjvalue_t data;
+  int16_t roc;
+  int16_t pvt;
+} plregobj_t;
+
+static plregobj_t maybe_fetch(plregobj_t maybe_onstack) {
+  if (maybe_onstack.roc != ROC_ONSTACK) {
+    return maybe_onstack;
+  }
+  
+  plregobj_t ret;
+  ret.roc = INREG;
+  ret.pvt = maybe_onstack.pvt;
+  plstkobj_t *stkobj = (plstkobj_t*)maybe_onstack.data.pvalue;
+  ret.data = stkobj->data;
+  return ret;
+}
+
 void eval_ast(ast_node_base_t *program) {
   (void)program;
 }
