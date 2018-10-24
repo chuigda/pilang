bd9b3a2d3b7e3ff4239609c903d49ed6ebf53bd2
ICEY <crazycz.cc@ckx-ily.cn>
2018-10-24 18:07:25 +0800

    Add fake template support

--- pilang/include/jjvalue.h
+++ pilang/include/jjvalue.h
@@ -6,6 +6,9 @@
 
 #include <stdint.h>
 
+#define TP(...)
+#define TEMPLATE(...)
+
 typedef union {
   int64_t ivalue;
   double fvalue;
@@ -23,20 +26,23 @@ typedef enum {
   JT_UNDEFINED
 } jjtype_t;
 
+TEMPLATE(T)
 typedef struct {
-  jjvalue_t value;
+  jjvalue_t TP(T) value;
   bool success;
 } result_t;
 
-static inline result_t failed_result(const char *error) {
-  result_t r;
+TEMPLATE(T)
+static inline result_t TP(T) failed_result(const char *error) {
+  result_t TP(T) r;
   r.success = 0;
   r.value.svalue = create_string(error);
   return r;
 }
 
-static inline result_t success_result(jjvalue_t value) {
-  result_t r;
+TEMPLATE(T)
+static inline result_t TP(T) success_result(jjvalue_t TP(T) value) {
+  result_t TP(T) r;
   r.success = 1;
   r.value = value;
   return r;
